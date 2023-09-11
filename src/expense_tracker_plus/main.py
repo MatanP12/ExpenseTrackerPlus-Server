b8cfafe406611bb6d8d163e77f8b1a77738c3891
Matan Peretz <matanperetz@outlook.com>
2023-09-11 18:11:03 +0300

    Create Transactions router
    - Update Transactions model, and create TransactionUpdate class.
    - Create Transactions router to Get, POST, PATCH, DELETE methods.

--- src/expense_tracker_plus/main.py
+++ src/expense_tracker_plus/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+
 from .models.db_connection import create_db_and_tables
+from .routes.transaction_router import transactions_router
 
 app = FastAPI()
+
+app.include_router(transactions_router)
 
 
 @app.on_event("startup")
--- src/expense_tracker_plus/models/db_connection.py
+++ src/expense_tracker_plus/models/db_connection.py
@@ -1,5 +1,4 @@
-from sqlmodel import create_engine, SQLModel
-
+from sqlmodel import SQLModel, create_engine
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
